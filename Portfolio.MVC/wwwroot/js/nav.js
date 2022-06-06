d3ae3209f937e1219ab7061bab814738f4f85ad9
Uzor Nwachukwu <uzor1997@gmail.com>
2022-06-06 11:50:23 +0100

    adding the close functionality to the navbar on navitem click

--- Portfolio.MVC/wwwroot/js/nav.js
+++ Portfolio.MVC/wwwroot/js/nav.js
@@ -1,4 +1,14 @@
 ﻿let nav = document.getElementById('navigation');
+let navItems = document.querySelectorAll(".navigation__item");
+
 function toggleMenu() {
     nav.classList.toggle('navigation--visible');
+}
+
+if (window.innerWidth < 992) {
+    navItems.forEach(item => {
+        item.addEventListener("click", () => {
+            nav.classList.toggle('navigation--visible');
+        })
+    })
 }
